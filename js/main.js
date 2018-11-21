d7e9ddebeb576c6d4d3daace84946416ff126b6e
hta-pete <pkatsaboulas@hightoweragency.com>
2018-11-21 09:15:59 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(function(){
+
+  var $window = $(window);
+
+
+
+  var lastScrollTop = $window.scrollTop();
+
+  function scrollStuff() {
+
+      var scrollTop = $window.scrollTop();
+      var y         = (scrollTop > lastScrollTop) ? 'down' : ((scrollTop === lastScrollTop) ? 'none' : 'up');
+      var bottom    = $window.height() + scrollTop == $(document).height();
+
+      if( y == 'down' && scrollTop > 70 ){
+
+          $('header').addClass('hide');
+          //$('.sub-menu--js').slideUp('fast');
+
+      } else{
+
+          $('header').removeClass('hide');
+
+      }
+      lastScrollTop = scrollTop;
+
+  }
+  scrollStuff();
+
+  $window.on('scroll', scrollStuff);
+
+
+
+  $('.hero-slides').slick({
+      autoplay:true,
+      autoplaySpeed:6000,
+      infinite:true,
+      dots:true,
+      slidesToShow:1,
+      slidesToScroll:1,
+      speed: 500,
+      pauseOnHover:false,
+      fade: true,
+      nextArrow: $('.slider-next-btn'),
+      prevArrow: $('.slider-prev-btn')
+  });
+
+
+  $('.has-sub-menu').on('mouseenter', function(){
+
+    $(this).find('.sub-menu').addClass('active');
+
+  });
+  $('.has-sub-menu').on('mouseleave', function(){
+
+    $(this).find('.sub-menu').removeClass('active');
+
+  });
+   
+
+});
+
+
+
+
+
+
+
